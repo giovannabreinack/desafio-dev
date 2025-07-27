b2537cf2c1651f12abd8174e93e88e6acb9fb0b7
Gih <giovannabreinackcolombara@gmail.com>
2025-07-27 16:22:53 -0300

    configurando ambiente de teste

---
+++ test/agricultor.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { App } from 'supertest/types';
+import { AppModule } from '../src/app.module';
+
+describe('Testes do Módulo Agricultor (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: "sqlite",
+          database: ":memory:",
+          entities: [__dirname + "./../src/**/entities/*.entity.ts"],
+          synchronize: true,
+          dropSchema: true
+        }),
+        AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
